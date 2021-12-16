928cf52f6271983526b32f1f9825b205e170181d
Sergio De Luca <sdlbsso@gmail.com>
2021-12-16 00:21:01 -0300

    rutas dinamicas

---
+++ src/main/java/com/sergio/empleos/controller/VacantesController.java
@@ -0,0 +1,22 @@
+package com.sergio.empleos.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/vacantes")
+public class VacantesController {
+
+		@GetMapping("/view/{id}")
+		public String verDetalle(@PathVariable("id") int idVacante, Model model) {
+			System.out.println("idvacante "+idVacante);
+			model.addAttribute("idVacante", idVacante);
+			
+			
+			//buscar el detalle en la bd
+			return "vacantes/detalle";
+		}
+}
