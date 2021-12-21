eeec97bc1e43eae1d0e2bc774f5c6c0123da2ee2
Sergio De Luca <sdlbsso@gmail.com>
2021-12-21 00:34:32 -0300

    Formulario creacion de vacante

--- src/main/java/com/sergio/empleos/controller/VacantesController.java
+++ src/main/java/com/sergio/empleos/controller/VacantesController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -14,23 +15,45 @@ import com.sergio.empleos.service.IVacanteService;
 @Controller
 @RequestMapping("/vacantes")
 public class VacantesController {
-	
-		@Autowired
-		private IVacanteService serviceVacantes;
-		
-		@GetMapping("/delete")
-	    public String eliminar(@RequestParam("id") int idVacante, Model model) {
-	    	System.out.println("Ide parametro recibido = "+idVacante);
-	    	model.addAttribute("id",idVacante);
-	    	return "vacantes/mensaje";
-	    }
-		
 
-		@GetMapping("/view/{id}")
-		public String verDetalle(@PathVariable("id") int idVacante, Model model) {
-			Vacante vacante = serviceVacantes.buscarPorId(idVacante);
-			model.addAttribute("vacante", vacante);
-			//buscar el detalle en la bd
-			return "detalle";
-		}
+	@Autowired
+	private IVacanteService serviceVacantes;
+
+	@GetMapping("/delete")
+	public String eliminar(@RequestParam("id") int idVacante, Model model) {
+		System.out.println("Ide parametro recibido = " + idVacante);
+		model.addAttribute("id", idVacante);
+		return "vacantes/mensaje";
+	}
+
+	@GetMapping("/crear")
+	public String crear() {
+		return "vacantes/formVacante";
+	}
+
+	@PostMapping("/guardar")
+	public String guardar(@RequestParam("nombre") String nombre, @RequestParam("descripcion") String descripcion,
+			@RequestParam("categoria") String categoria, @RequestParam("estatus") String estatus,
+			@RequestParam("fecha") String fecha, @RequestParam("destacado") int destacado,
+			@RequestParam("salario") double salario, @RequestParam("detalles") String detalle) {
+		
+		System.out.println("Nombre "+nombre);
+		System.out.println("Descripcion "+descripcion);
+		System.out.println("Categoria "+categoria);
+		System.out.println("Estado "+estatus);
+		System.out.println("Fecha"+fecha);
+		System.out.println("Destacado "+destacado);
+		System.out.println("Salario "+salario);
+		System.out.println("Detalle "+detalle);
+		
+		return "vacantes/listaVacantes";
+	}
+
+	@GetMapping("/view/{id}")
+	public String verDetalle(@PathVariable("id") int idVacante, Model model) {
+		Vacante vacante = serviceVacantes.buscarPorId(idVacante);
+		model.addAttribute("vacante", vacante);
+		// buscar el detalle en la bd
+		return "detalle";
+	}
 }
